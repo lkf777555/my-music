38c619d80cbe497649db6e6b3d83dbd95be92fc0
lkf <1656399881@qq.com>
2023-09-08 08:53:44 +0800

    调整

--- src/components/my-swiper/my-swiper.vue
+++ src/components/my-swiper/my-swiper.vue
@@ -1,38 +1,44 @@
 <template>
-  <div>
-    <el-skeleton :loading="vloading" :animated="true">
-      <template #default>
-        <el-carousel :interval="3000" type="card" height="200px">
-          <el-carousel-item v-for="item in list" :key="item">
-            <img class="swiper-img" :src="item.imageUrl" />
-          </el-carousel-item>
-        </el-carousel>
-      </template>
-      <template #template>
-        <el-skeleton-item variant="p" style="width: 27%; height: 200px" />
-        <el-skeleton-item variant="p" style="width: 46%; height: 200px" />
-        <el-skeleton-item variant="p" style="width: 27%; height: 200px" />
-      </template>
-    </el-skeleton>
-  </div>
+    <div>
+        <el-skeleton :loading="vloading" :animated="true">
+            <template #default>
+                <el-carousel :interval="3000" type="card" height="200px">
+                    <el-carousel-item v-for="item in list" :key="item" @click="pushClick(item)">
+                        <img class="swiper-img" :src="item.imageUrl" />
+                    </el-carousel-item>
+                </el-carousel>
+            </template>
+            <template #template>
+                <el-skeleton-item variant="p" style="width: 27%; height: 200px" />
+                <el-skeleton-item variant="p" style="width: 46%; height: 200px" />
+                <el-skeleton-item variant="p" style="width: 27%; height: 200px" />
+            </template>
+        </el-skeleton>
+    </div>
 </template>
 
 <script setup>
 let props = defineProps({
-  list: {
-    type: Array,
-    default: () => {},
-  },
-  vloading: {
-    type: Boolean,
-    default: true,
-  },
+    list: {
+        type: Array,
+        default: () => {},
+    },
+    vloading: {
+        type: Boolean,
+        default: true,
+    },
 });
+
+let vm = inject("$vm");
+
+const pushClick = (item) => {
+    vm.MsgSuccess("待开发");
+};
 </script>
 <style lang="scss" scoped>
 .swiper-img {
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
 }
 </style>
